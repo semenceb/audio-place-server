b2eceb9f5c6321f67fd7f4c220f440182ae80fad
semenceb <gigltitlggg@gmail.com>
2026-09-05 20:35:29 +0500

    add dev/prod envs

--- src/app.module.ts
+++ src/app.module.ts
@@ -13,10 +13,16 @@ import { PlaylistsModule } from './modules/playlists';
 import { SongModule } from './modules/song';
 import { PlaybackQueueModule } from './modules/playback-queue';
 
+console.log(process.env.NODE_ENV);
+
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
+      envFilePath:
+        process.env.NODE_ENV === 'development'
+          ? '.env.development'
+          : '.env.production',
     }),
     ServeStaticModule.forRoot({
       rootPath: join(process.cwd(), 'uploads'),
